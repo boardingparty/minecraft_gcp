0bb1a781714488aeca53359cab1ecc7d7e0a13ee
Tom Larkworthy <tom.larkworthy@gmail.com>
2020-06-26 11:01:37 +0200

    Initial commit

---
+++ terraform/variables_backend_gcp.tf
@@ -0,0 +1,19 @@
+variable "tf_state_bucket" {
+  type = string
+}
+
+variable "project" {
+  type = string
+}
+
+variable "application" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
