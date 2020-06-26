12014cdb0487da213b32898cfb402d2ae5c6b085
Tom Larkworthy <tom.larkworthy.external@zalando.de>
2020-06-26 13:15:56 +0200

    Export zone properly

--- terraform/variables_minecraft.tf
+++ terraform/variables_minecraft.tf
@@ -1,20 +1,20 @@
 variable "zone" {
-  type = string
-  default = ""
+  type     = string
+  required = true
 }
 
 variable "startup_script" {
-    type = string
-    value = "docker run -d -p 25565:25565 -e EULA=TRUE -v /var/minecraft:/data --name mc -e TYPE=FORGE -e MEMORY=2G --rm=true itzg/minecraft-server:latest;"
+  type    = string
+  default = "docker run -d -p 25565:25565 -e EULA=TRUE -v /var/minecraft:/data --name mc -e TYPE=FORGE -e MEMORY=2G --rm=true itzg/minecraft-server:latest;"
 }
 
 variable "enable_switch_access_group" {
-  type = number
+  type    = number
   default = 0
 }
 
 variable "minecraft_switch_access_group" {
-  type = string
+  type    = string
   default = ""
 }
 
